0636e25cfd0217018873d9751f3873689c0015be
zyf <zhaoyafei0210@gmail.com>
2016-10-25 02:38:12 +0800

    fix indentation of get_webfiles.py

--- get_webfiles.py
+++ get_webfiles.py
@@ -6,17 +6,17 @@ import subprocess
 
 def wget_files(filelist,outpath):
     '''Download data'''
+    
+    print 'filelist is: ', filelist
+    print 'output path is: ', outpath
 
-	print 'filelist is: ', filelist
-	print 'output path is: ', outpath
+    if not osp.exists(filelist):
+        print 'Cannot find filelist: ', filelist
+        return
 
-	if not osp.exist(filelist):
-		print 'Cannot find filelist: ', filelist
-		return
-	
-	f = open(filelist,'r')
-    log= open(osp.splitext(filelist)[0]+'_log.txt','w')
-
+    f = open(filelist,'r')
+    log= open(osp.splitext(filelist)[0] + '_log.txt','w')
+    
     
     if not osp.exists(outpath):
         os.mkdir(outpath)
@@ -27,23 +27,23 @@ def wget_files(filelist,outpath):
         try:
             webfile = str(i.strip())
             basename = osp.basename(webfile)
-            cmd = 'wget '+ webfile    
+            cmd = 'wget ' +  webfile    
             print(cmd)            
-            if not osp.exists(outpath+basename):
+            if not osp.exists(outpath + basename):
                 status = subprocess.call(cmd)
                 if status !=0:
-                    log.write('\nFailed:'+webfile)
+                    log.write('\nFailed:' + webfile)
                     continue
-                log.write('\nSuccess:'+webfile)
+                log.write('\nSuccess:' + webfile)
             log.flush()
         except:
-            log.write('\nFailed:'+webfile)
+            log.write('\nFailed:' + webfile)
             continue        
     f.close()
     log.close()
 
 if __name__  =='__main__':
-        outpath = './test-images-100'
-        file_list = r'./tantan-fd.txt'
-        wget_files(file_list,outpath)
-        print('END')
+    outpath = './test-images-100'
+    file_list = r'./tantan-fd.txt'
+    wget_files(file_list,outpath)
+    print('END')
