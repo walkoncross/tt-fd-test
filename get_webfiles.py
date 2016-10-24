460828244f4932c0166dd94be34fe0fc25becf76
zyf <zhaoyafei0210@gmail.com>
2016-10-25 02:19:30 +0800

    add get_webfiles.py tantan-fd-list.txt tantan-fd-list.txt

---
+++ get_webfiles.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#coding:utf-8
+import os
+import os.path as osp
+import subprocess
+
+def wget_files(filelist,outpath):
+    '''Download data'''
+    f = open(filelist,'r')
+    log= open(osp.splitext(filelist)[0]+'_log.txt','w')
+    
+    if not osp.exists(outpath):
+        os.mkdir(outpath)
+        
+    os.chdir(outpath)
+    print(os.curdir)
+    for i in f:
+        try:
+            webfile = str(i.strip())
+            basename = osp.basename(webfile)
+            cmd = 'wget '+ webfile    
+            print(cmd)            
+            if not osp.exists(outpath+basename):
+                status = subprocess.call(cmd)
+                if status !=0:
+                    log.write('\nFailed:'+webfile)
+                    continue
+                log.write('\nSuccess:'+webfile)
+            log.flush()
+        except:
+            log.write('\nFailed:'+webfile)
+            continue        
+    f.close()
+    log.close()
+
+if __name__  =='__main__':
+        outpath = './test-images-100'
+        file_list = r'./tantan-fd.txt'
+        wget_files(file_list,outpath)
+        print('END')
